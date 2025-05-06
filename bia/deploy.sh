4c0ffa2e2329d8f65a21e8c41d39e6026d022eb8
wagner <vvagner.silva@gmail.com>
2025-05-06 20:13:13 -0300

    Ajustes nas conexoes do RDS

--- bia/deploy.sh
+++ bia/deploy.sh
@@ -1,2 +1,2 @@
 ./build.sh
-aws ecs update-service --cluster cluster-bia --service task-def-bia-service --force-new-deployment
+aws ecs update-service --cluster cluster-bia --service service_bia --force-new-deployment
