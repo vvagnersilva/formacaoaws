1c4abc05e6ed2f1c7a7348a074a6ac91c113d0ee
EC2 Default User <ec2-user@ip-172-31-0-203.ec2.internal>
2026-08-30 22:41:27 +0000

    Alteracoes para CI/CD

--- bia/client/src/components/AddTask.jsx
+++ bia/client/src/components/AddTask.jsx
@@ -59,7 +59,7 @@ const AddTask = ({ onAdd }) => {
       </div>
       
       <button type="submit" className="btn btn-block success">
-        Add Task com ALB
+        Add Task com CI/CD
       </button>
       
       <Modal
