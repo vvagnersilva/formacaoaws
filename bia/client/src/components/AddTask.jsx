d89e3dd1fe1af58e40ef0c5c909dea533b5c46ec
Wagner Silva <vvagner.silva@gmail.com>
2026-08-30 19:37:40 -0300

    Alteracoes para CI CD.

--- bia/client/src/components/AddTask.jsx
+++ bia/client/src/components/AddTask.jsx
@@ -59,7 +59,7 @@ const AddTask = ({ onAdd }) => {
       </div>
       
       <button type="submit" className="btn btn-block success">
-        Add New Task
+        Add Task com CI/CD 
       </button>
       
       <Modal
--- bia/deploy.sh
+++ bia/deploy.sh
@@ -1,23 +1,2 @@
-#!/bin/bash
-
-AMBIENTE=$1
-API_URL="http://34.238.52.190"
-
-echo "Vou iniciar o deploy no Ambiente: "$AMBIENTE
-echo "O endereço da API é: "$API_URL
-
-if [ "$AMBIENTE" != "hom" ] && [ "$AMBIENTE" != "prod" ]; then
-    echo "Ambiente inválido"
-    exit 1
-fi
-
-echo "Fazendo deploy ..."
-
-. react.sh
-source s3.sh
- 
-build $API_URL
-
-envio_s3
-
-echo "Deploy Finalizado"
+./build.sh
+aws ecs update-service --cluster [SEU_CLUSTER] --service [SEU_SERVICE]  --force-new-deployment
